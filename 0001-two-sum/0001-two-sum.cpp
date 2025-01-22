b5a7e2f1fe6d7938d397bf8052fadf42c1993bbf
sara farouk <126704906+sara-mohamd@users.noreply.github.com>
2025-01-22 22:59:22 +0200

    Time: 145 ms (5.08%), Space: 14.1 MB (75.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,18 @@
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> sol;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int temp = target - nums[i];
-            for(int j = i + 1; j < n; j++) {
-                if (nums[j] == temp){
-                sol.push_back(i);
-                sol.push_back(j);
-                return sol;
-                }
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++) {
+            auto it = find(nums.begin(), nums.end(), target - nums[i]) - nums.begin();
+            if(find(nums.begin(), nums.end(), target - nums[i]) != nums.end() && it != i) {
+                ans.push_back(i);
+                ans.push_back(it);
+                break;
             }
+            else
+                continue;
         }
-        return sol;
+        return ans;
     }
 };
