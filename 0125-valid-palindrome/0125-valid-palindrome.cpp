67f9cd6bcf6f07e5642521a0266a33035abc789c
sarah♡ <126704906+sara-mohamd@users.noreply.github.com>
2025-07-14 23:42:37 +0300

    Time: 7 ms (6.8%), Space: 10 MB (58.38%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        int n = s.length();
-        string str = "", rev_str;
-        for (int i = 0; i < n; i++) {
-            char c = (char)tolower(s[i]);
-            if((c >= 'a' && c <= 'z') || (s[i] >= '0' && s[i] <= '9'))
-                str += c;
-        }
+    /*  straightforward implementation  */
+    // bool isPalindrome(string s) {
+    //     int n = s.length();
+    //     string str = "", rev_str;
+    //     for (int i = 0; i < n; i++) {
+    //         char c = (char)tolower(s[i]);
+    //         if((c >= 'a' && c <= 'z') || (s[i] >= '0' && s[i] <= '9'))
+    //             str += c;
+    //     }
 
-        n = str.size(), rev_str = str;
-        for (int i = 0; i * 2 < n; i++) {
-            swap(rev_str[i], rev_str[n - (i + 1)]);
+    //     n = str.size(), rev_str = str;
+    //     for (int i = 0; i * 2 < n; i++) {
+    //         swap(rev_str[i], rev_str[n - (i + 1)]);
+    //     }
+    //     return (rev_str == str);
+    // }
+
+    /*  Two pointer implementation  */
+    bool isPalindrome(string s) {
+        int start = 0, end = s.length() - 1;
+        while (start <= end) {
+            if(!isalnum(s[start])) { start++; continue; }
+            if(!isalnum(s[end])) { end--; continue; }
+            if(tolower(s[start]) != tolower(s[end])) { return false; }
+            else
+                start++, end--;
         }
-        // cout << "reg " << str << "\nrev " << rev_str << '\n';
-        return (rev_str == str);
+        return true;
     }
 };
