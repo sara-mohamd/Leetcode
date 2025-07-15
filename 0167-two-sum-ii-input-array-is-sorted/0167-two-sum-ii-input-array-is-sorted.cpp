29d992a42cf9d979c5fc6a971e68f8a2b8957066
sarah♡ <126704906+sara-mohamd@users.noreply.github.com>
2025-07-15 19:34:32 +0300

    Time: 0 ms (100%), Space: 19.5 MB (63.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -4,16 +4,13 @@ public:
         vector<int> ans;
         int l = 0, r = nums.size() - 1;
         while(l <= r) {
-            if( nums[l] + nums[r] == target) {
-                ans.push_back(l + 1),
-                ans.push_back(r + 1);
-                break;
-            }
+            if( nums[l] + nums[r] == target)
+                return vector<int> {l + 1, r + 1};
             if(nums[l] + nums[r] < target)
                 l++;
             else
                 r--; 
         }
-        return ans;
+        return vector<int>();
     }
 };
