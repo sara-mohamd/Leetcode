067ae94ea9f6d32fbb1e96183facd04d5d26e874
sarah♡ <126704906+sara-mohamd@users.noreply.github.com>
2025-07-15 12:21:25 +0300

    Time: 16 ms (5.86%), Space: 26.7 MB (24.12%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     bool isPalindrome(string s, int l, int r) {
         while(l <= r) {
-            // cout << s[l] << ' ' << s[r] << '\n';
             if (s[l++] != s[r--])
                 return false;
         }
         return true;
     }
     bool validPalindrome(string s) {
-        int chance = 1, start = 0, end = s.size() - 1;
+        int  start = 0, end = s.size() - 1;
         while (start <= end) {
             if(s[start] == s[end])
                 start++, end--;
-            else if(s[start] != s[end] && chance) {
-                // cout << start << ' ' << end << '\n';
-                if(isPalindrome(s, start + 1, end)) {
-                    start++, chance = 0;
-                }
+            else if(s[start] != s[end]) {
+                if(isPalindrome(s, start + 1, end))
+                    return true;
                 else if (isPalindrome(s, start, end - 1))
-                    end--, chance = 0;
-                else {
+                    return true;
+                else
                     return false;
-                }
             }
             else
                 return false;
