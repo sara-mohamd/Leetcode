249a66d0d91ef83f6c03ce4a047a849e275d63a0
sarah♡ <126704906+sara-mohamd@users.noreply.github.com>
2025-07-16 15:46:46 +0300

    Time: 73 ms (64.5%), Space: 98.7 MB (42.94%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-  bool containsNearbyDuplicate(vector<int> &nums, int k) {
-    unordered_map<int, int> lastSeen;
+  /* -------- data structure–based solution ---------- */
+//   bool containsNearbyDuplicate(vector<int> &nums, int k) {
+//     unordered_map<int, int> lastSeen;
 
-    for (int i = 0; i < nums.size(); i++) {
-      if (lastSeen.count(nums[i]) && i - lastSeen[nums[i]] <= k)
+//     for (int i = 0; i < nums.size(); i++) {
+//       if (lastSeen.count(nums[i]) && i - lastSeen[nums[i]] <= k)
+//         return true;
+//       lastSeen[nums[i]] = i;
+//     }
+//     return false;
+//   }
+
+ bool containsNearbyDuplicate(vector<int> &nums, int k) {
+    int l = 0, r = 0, n = nums.size();
+    unordered_map<int, int> freq;
+    while (l < n) {
+        while (r <= (k + l) and r < n)
+    {
+        freq[nums[r]]++;
+            if (freq[nums[r]] == 2)
         return true;
-      lastSeen[nums[i]] = i;
+        r++;
+    }
+        freq[nums[l++]]--;
     }
     return false;
-  }
+ }
 };
