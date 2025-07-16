c5868bc14c8b9ed7214b7f1854957a8f7aaa5ab2
sarah♡ <126704906+sara-mohamd@users.noreply.github.com>
2025-07-16 21:13:12 +0300

    Time: 0 ms (100%), Space: 13.7 MB (5.8%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,8 +1,23 @@
 #define all(x) (x).begin(), (x).end()
+#define sz(x) (x).size()
 class Solution {
 public:
+    /* -- Using built in Binary search function -- */
+    // int searchInsert(vector<int>& nums, int target) {
+    //     auto it = lower_bound(all(nums), target) - nums.begin();
+    //     return it;
+    // }
+
+    /* -- build lower_bound function -- */
     int searchInsert(vector<int>& nums, int target) {
-        auto it = lower_bound(all(nums), target) - nums.begin();
-        return it;
+        int l = 0, r = sz(nums) - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return r + 1;
     }
 };
